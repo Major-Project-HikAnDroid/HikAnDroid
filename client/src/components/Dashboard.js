d8f27a9b75770a2dd29219698ff3096ee3d8c33a
ansh8tu <anshu8tu@gmail.com>
2022-10-27 22:00:24 +0530

    Added forgot-password, private route

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+ 
 export default function Dashboard() {
+    const [error, setError] = useState("");
+    const { currentUser, logout } = useAuth();
+    const navigate = useNavigate()
+
+    async function handleLogout(){
+        setError("");
+        try{
+            await logout();
+            navigate("/login");
+        }catch{
+            setError("Failed to log out");
+        }
+    }
+
     return (
         <>
-            <h1>Dashboard</h1>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">About Me</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>Email: </strong>{currentUser.email}<br/>
+                    <strong>Applications Published: </strong>{"0"}<br/><br/>
+                    <Link to="/update-profile" className="btn btn-primary w-100 mt-3"> Update Profile</Link>
+                    <Link to="/publish-application" className="btn btn-primary w-100 mt-3"> Publish Application</Link>
+                    <Link to="/application-status" className="btn btn-primary w-100 mt-3"> Application Status</Link>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                <Button variant="link" onClick={handleLogout}>Log Out</Button>
+            </div>
         </>
     )
 }
