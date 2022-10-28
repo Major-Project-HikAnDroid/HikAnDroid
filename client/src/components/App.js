65f9680e7b3936a671d75b25706e5c9a27f7324a
ansh8tu <anshu8tu@gmail.com>
2022-10-28 19:36:35 +0530

    Added background image

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,7 +11,7 @@ import UpdateProfile from "./UpdateProfile";
 
 function App() {
   return (
-    
+  
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
         <div className="w-100" style={{ maxWidth: "400px" }}>
           <Router> 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,6 +2,7 @@ import React, {useRef, useState} from "react";
 import {Form, Button, Card, Alert} from "react-bootstrap";
 import {useAuth} from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
+import '../styles/signup.css'
 
 
 export default function Signup(){
@@ -34,31 +35,33 @@ export default function Signup(){
     
     return(
         <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Sign Up</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required />
-                        </Form.Group>
-                        <br></br>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} required />
-                        </Form.Group>
-                        <br></br>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Confirm Password</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required />
-                        </Form.Group>
-                        <br></br>
-                        <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className="w-100 text-center mt-2">
+            <div className="signup-div">
+                <Card className="opacity-50">
+                    <Card.Body>
+                        <h2 className="text-center mb-4">Sign Up</h2>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group id="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" ref={emailRef} required />
+                            </Form.Group>
+                            <br></br>
+                            <Form.Group id="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" ref={passwordRef} required />
+                            </Form.Group>
+                            <br></br>
+                            <Form.Group id="password-confirm">
+                                <Form.Label>Confirm Password</Form.Label>
+                                <Form.Control type="password" ref={passwordConfirmRef} required />
+                            </Form.Group>
+                            <br></br>
+                            <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="w-100 text-center mt-2 text-white">
                 Already have an account? <Link to = "/login">Log In</Link>
             </div>
         </>
